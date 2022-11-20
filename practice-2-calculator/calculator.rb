774b3ad1672652ff8cd4c36867681b5d1c1804b7
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-11-20 21:57:30 +0200

    rename folders to contain more info

---
+++ practice-2-calculator/calculator.rb
@@ -0,0 +1,14 @@
+require_relative 'string_parsing'
+require_relative 'calculate'
+
+# User Input
+puts 'Enter expression with 2 variables ex(2.3 + 61): '
+user_str = STDIN.gets.gsub(/\s+/, '') # removes all white spaces
+raise 'No input found!' if user_str.empty?
+
+operation = parse_string(user_str)
+res = calculate(operation[:operator], operation[:operand_l], operation[:operand_r])
+puts "#{operation[:operand_l]} \
+    #{operation[:operator]} \
+    #{operation[:operand_r]} = \
+    #{res}"
---
+++ practice-3-brackets-count/main.rb
@@ -0,0 +1,48 @@
+
+open_round_brackets = 0
+open_square_brackets = 0
+open_curly_brackets = 0
+
+while true 
+    # User Input
+    begin
+        puts 'Enter first bracket symbol or exit:'
+
+        user_str = STDIN.gets.gsub(/\s+/, '')
+        break if user_str == 'exit'
+
+        # remove all non-bracket symbols
+        user_chr = user_str.gsub(/[^{}()\[\]]/, '')
+        raise 'Invalid input' if user_chr.length != 1
+
+    rescue => e
+        puts "You entered not 1, but #{user_chr.length} symbols at once\nPlease retry:\n\n" if user_chr.length > 1
+        puts "You entered no valid symbol" if user_chr.length === 0
+        next
+    end
+
+    case user_chr
+    when '('
+        open_round_brackets+=1
+    when ')'
+        open_round_brackets-=1
+     when '['
+        open_square_brackets+=1
+    when ']'
+        open_square_brackets-=1
+    when '{'
+        open_curly_brackets+=1
+    when '}'
+        open_curly_brackets-=1
+    else 
+        # should not be possible to raise, but what if...
+        raise 'Internal logic error'
+    end
+    raise 'Illigal number of curly brackets!' if open_curly_brackets < 0
+    raise 'Illigal number of round brackets!' if open_round_brackets < 0
+    raise 'Illigal number of square brackets!' if open_square_brackets < 0
+    
+end
+puts 'Ending program before switch...'
+
+def brackets_calculator ()
