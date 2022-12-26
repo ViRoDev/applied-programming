024c9c8a36fa673944687d37e69cf3d03a92b7f7
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-12-27 01:09:49 +0200

    add drawing of stick figure & counting failed guesses

--- hangman/main.rb
+++ hangman/main.rb
@@ -27,9 +27,33 @@ def hangman
 
     loop do
         puts render_word(word, guessed_letters)
+        puts render_hangman(parts_of_hangman_drawn)
         letter = gets.chomp
+        if (!word.include?(letter)) 
+            parts_of_hangman_drawn = parts_of_hangman_drawn + 1
+            puts "There is no such letter in this word"
+        end
         guessed_letters.push(letter)
     end
+end
+
+def render_hangman(wrong_answers)
+        rope = '|' if wrong_answers > 0
+        head = 'o' if wrong_answers > 1
+        torso = '()' if wrong_answers > 2
+        r_hand = '/' if wrong_answers > 3 
+        l_hand = '\\' if wrong_answers > 4
+        r_leg = '/' if wrong_answers > 5
+        l_leg = '\\' if wrong_answers > 6
+    hangman = "
+    ------
+    |   #{rope}
+    |   #{head}
+    | #{r_hand != nil ? r_hand : " "}#{torso}#{l_hand}
+    |  #{r_leg}#{l_leg}
+    +-----
+    "
+    return hangman 
 end
 
 def render_word(word, guessed_letters)
