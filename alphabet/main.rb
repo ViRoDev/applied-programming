d817112a20197c2c0c6d2977700c6aaca0982b73
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-12-27 00:06:19 +0200

    complete alphabet transformation

--- alphabet/main.rb
+++ alphabet/main.rb
@@ -1,6 +1,14 @@
+POSITION_OF_LOWERCASE_A_IN_ASCII = 97
 def alphabet_position(string) 
-    puts string.chars()
+    lower = string.downcase
+    letters = lower.scan /[a-z]/
+
+    numbers = letters.map do |l| 
+        # adding one to make 'a' = 1, and not 'a' = 0
+        l.ord - POSITION_OF_LOWERCASE_A_IN_ASCII + 1
+    end
+    return numbers.join(' ')
 end
 
-
-alphabet_position("hello")
+# should be a test
+puts alphabet_position("The sunset sets at twelve o'clock.")
