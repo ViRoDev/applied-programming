7f6e0183829d61879dd1bd9efa78ee1e845d0f5e
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-12-27 03:55:38 +0200

    move all testing into tests.rb

--- practice-4-top-three-words/top_three.rb
+++ practice-4-top-three-words/top_three.rb
@@ -1,14 +1,4 @@
 # frozen_string_literal: true
-
-text = "In a#village234 of La Mancha,\
- the name of which I have no desire to call to mind, \
- there lived not long since one of those gentlemen that keep a lance in the lance-rack, \
- an old buckler, a lean hack, and a greyhound for coursing. \
- An olla of rather more beef than mutton, \
- a salad on most nights, scraps on Saturdays, \
- lentils on Fridays, and a pigeon or so extra on Sundays, \
- made away with three-quarters of his income."
-
 def find_top_three_words(text)
   all_words = text.downcase
                   .gsub(/[^(a-z)\s']/, ' ')
@@ -25,4 +15,3 @@ def find_top_three_words(text)
   return top_three
 end
 
-puts find_top_three_words(text)
