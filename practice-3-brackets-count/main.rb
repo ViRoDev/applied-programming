b98c4c0c74cbbdd37e4a91f33d79d012abbc2e17
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-12-27 02:09:20 +0200

    rewrite brackets count to be more functional

--- practice-3-brackets-count/main.rb
+++ practice-3-brackets-count/main.rb
@@ -1,47 +1,36 @@
+def check_if_brackets_correct(string)
+  open_round_brackets = 0
+  open_square_brackets = 0
+  open_curly_brackets = 0
 
-open_round_brackets = 0
-open_square_brackets = 0
-open_curly_brackets = 0
-
-while true 
-    # User Input
-    begin
-        puts 'Enter *one* bracket symbol or exit:'
-
-        user_str = STDIN.gets.gsub(/\s+/, '')
-        break if user_str == 'exit'
-
-        # remove all non-bracket symbols
-        user_chr = user_str.gsub(/[^{}()\[\]]/, '')
-        raise 'Invalid input' if user_chr.length != 1
-
-    rescue => e
-        puts "You entered not 1, but #{user_chr.length} symbols at once\nPlease retry:\n\n" if user_chr.length > 1
-        puts "You entered no valid symbol" if user_chr.length === 0
-        next
-    end
-
-    case user_chr
-    when '('
+  string.split('').each do |symbol|
+    case symbol
+      when '('
         open_round_brackets+=1
-    when ')'
+      when ')'
         open_round_brackets-=1
-     when '['
+       when '['
         open_square_brackets+=1
-    when ']'
+      when ']'
         open_square_brackets-=1
-    when '{'
+      when '{'
         open_curly_brackets+=1
-    when '}'
+      when '}'
         open_curly_brackets-=1
     else 
         # should not be possible to raise, but what if...
-        raise 'Internal logic error'
+        raise "Symbol #{symbol} not supported"
     end
-    raise 'Illigal number of curly brackets!' if open_curly_brackets < 0
-    raise 'Illigal number of round brackets!' if open_round_brackets < 0
-    raise 'Illigal number of square brackets!' if open_square_brackets < 0
-    
+
+    return false  if open_curly_brackets < 0 || open_round_brackets < 0  || open_square_brackets < 0
+  end
+
+  return true if open_curly_brackets == 0 && open_round_brackets == 0 && open_square_brackets == 0
+
+  return false 
 end
-puts 'Ending program before switch...'
+
+test_1 = "[[]]]()"
+
+puts check_if_brackets_correct(test_1)
 
