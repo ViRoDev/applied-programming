542e67d286c1ccf6dd4e411120967c8bb87b1631
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-12-27 02:20:55 +0200

    add unit tests

--- practice-3-brackets-count/main.rb
+++ practice-3-brackets-count/main.rb
@@ -30,7 +30,33 @@ def check_if_brackets_correct(string)
   return false 
 end
 
-test_1 = "[[]]]()"
+require 'test/unit'
+class TestBrackets < Test::Unit::TestCase
+    def test_one_pair_square
+        assert_equal true, check_if_brackets_correct('[]')
+    end
+    
+    def test_one_pair_curly
+        assert_equal true, check_if_brackets_correct('{}')
+    end
 
-puts check_if_brackets_correct(test_1)
+    def test_one_pair_round
+        assert_equal true, check_if_brackets_correct('()')
+    end
 
+    def test_equal
+        assert_equal true, check_if_brackets_correct('({})')
+        assert_equal true, check_if_brackets_correct('{[()]}')
+        assert_equal true, check_if_brackets_correct('[{}(())]')
+    end
+
+    def test_not_closed
+        assert_equal false, check_if_brackets_correct('[[')
+        assert_equal false, check_if_brackets_correct('[[]')
+    end
+
+    def test_not_opened
+        assert_equal false, check_if_brackets_correct('[]{)}')
+        assert_equal false, check_if_brackets_correct('){}{}')
+    end
+end
